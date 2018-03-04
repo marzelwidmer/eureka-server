c011b854f528495f99e05e1d846d88fa82abd431
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-04 22:54:26 +0100

    initial commit

---
+++ src/main/java/ch/keepcalm/microservice/eureka/EurekaServer.java
@@ -0,0 +1,18 @@
+package ch.keepcalm.microservice.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
+public class EurekaServer {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
+
